5b681451fd358f1a12a8883118b42400dbe5bb41
Shubham Singh <shpsingh@amazon.com>
2023-06-07 18:32:34 +0530

    June_07 C2

---
+++ June_07/C2.js
@@ -0,0 +1,137 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+// class Test {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// //object 
+// const test_class = new Test("shubham")
+// console.log(test_class.name);
+
+
+//getters and setters
+
+
+// class Person {
+//     constructor(first_name, age, last_name) {
+//         this.first_name = first_name;
+//         this.age = age;
+//         this.last_name = last_name;
+//     }
+    
+//     get name() {
+//         return this.first_name +" "+ this.last_name;
+//     }
+    
+//     set name(name) {
+//         this.first_name = name.split(" ")[0]
+//         this.last_name = name.split(" ")[1]
+//     }
+    
+// }
+
+
+// let test_person = new Person("shubham", 23, "singh");
+// test_person.name = "Test Person"
+// console.log(test_person.name)
+
+
+// class expressions
+// let  Person = class {
+//     constructor(name) {
+//         this.name = name
+//     }
+// }
+
+
+// const test_person = new Person("shubham singh")
+// console.log(test_person)
+
+
+
+
+// async and sync functions
+
+
+
+
+
+// console.log("before fxn")
+// function test_fxn(a, b,c) {
+//     console.log(a, b, c);
+// }
+
+// test_fxn(10,20,30)
+// console.log("after the fxn")
+
+
+
+// console.log("before fxn")
+// async function test_fxn(a, b,c) {
+//     return a+b+c
+// }
+
+// console.log(test_fxn(10,20,30))
+// console.log("after the fxn")
+
+
+// console.log("before fxn")
+// async function test_fxn(a, b,c) {
+//     return a+b+c
+// }
+
+// console.log(await test_fxn(10,20,30))
+// console.log("after the fxn")
+
+// console.log("before fxn")
+// async function test_fxn(a, b,c) {
+//     return a+b+c
+// }
+
+// console.log(await test_fxn(10,20,30))
+// console.log("after the fxn")
+
+
+// (function(a){
+//     return (function(){
+//       console.log(a);
+//       a = 6;
+//     })()
+//    })(21);
+
+
+// const obj1 = {first: 20, second: 30, first: 50};
+// console.log(obj1);
+
+
+// var a = true + true + true * 3;
+
+// var a = 1 + 1 + 1*3
+// print(a)
+
+
+// const obj1 = {Name: "Hello", Age: 16};
+// const obj2 = {Name: "Hello", Age: 16};
+// print(obj1 === obj2);
+
+
+// function dog() {
+//     print("I am a dog.");
+//  }
+//  dog.sound = "Bark";
+
+
+
+
+
+
+
+
+
+
+
